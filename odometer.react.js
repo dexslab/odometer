1a3e74c4ee62727456d47a6013b3d580a04ba7c0
Adrian Cruz <adr.ca@live.com>
2020-09-17 22:42:08 -0700

    react component

---
+++ odometer.react.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Odometer from './odometer';
+
+export default class ReactOdometer extends PureComponent {
+  static propTypes = {
+    animation: PropTypes.bool,
+    auto: PropTypes.bool,
+    duration: PropTypes.number,
+    format: PropTypes.string,
+    selector: PropTypes.string,
+    theme: PropTypes.string,
+    value: PropTypes.number.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.node = React.createRef();
+  }
+
+  componentDidMount() {
+    const { value, ...options } = this.props;
+    this.odometer = new Odometer({
+      el: this.node.current,
+      value,
+      ...options,
+    });
+  }
+
+  componentDidUpdate() {
+    const { value } = this.props;
+    this.odometer.update(value);
+  }
+
+  render() {
+    return React.createElement('div', {
+      ref: this.node,
+    });
+  }
+}
